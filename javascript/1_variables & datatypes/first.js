92178164d524f505262654c1a395b8bb0ab5192c
sandipsah-git <sandip123456sah@gmail.com>
2025-10-12 15:50:48 +0545

    commit

--- javascript/1_variables & datatypes/first.js
+++ javascript/1_variables & datatypes/first.js
@@ -4,12 +4,15 @@
 
 let a = 10;
 
-a = 15;
+a = 15; // can re-assign
+
+// let a = 30; --> can't re-define
 
 console.log(a);
 
 // const 
 
+// x = 370;  (0ps , its also working)
 const x = 30;
 
 console.log(x);
@@ -31,3 +34,37 @@ console.log(b);
 
 console.log(typeof(z));
 console.log(typeof(a));
+
+
+// examples 1
+console.log();
+
+///////////////////////
+const person = {
+    name: 'Mayank',
+    age: 21
+};
+
+console.log(person.name);    // Output: Mayank
+
+// Modifying object properties is allowed
+person.name = 'Love';
+console.log(person.name);    // Output: Love
+
+// Attempting to reassign the variable will throw an error
+person = {};    // Throws TypeError: Assignment to constant variable.
+
+
+//  example 2
+
+////////////////
+
+const numbers = [1, 2, 3];
+console.log(numbers);    // Output: [1, 2, 3]
+
+// Modifying array elements is allowed
+numbers.push(4);
+console.log(numbers);    // Output: [1, 2, 3, 4]
+
+// Attempting to reassign the variable will throw an error
+numbers = []; // Throws TypeError: Assignment to constant variable.
