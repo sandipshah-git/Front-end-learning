90a8089a43186353ba23a2e16b343f54866d5965
sandipsah-git <sandip123456sah@gmail.com>
2025-10-12 15:15:27 +0545

    commit

---
+++ javascript/1_variables & datatypes/first.js
@@ -0,0 +1,33 @@
+// variables........
+
+// let
+
+let a = 10;
+
+a = 15;
+
+console.log(a);
+
+// const 
+
+const x = 30;
+
+console.log(x);
+
+// var
+
+var b = 40;
+b = 90;
+var b = 80;
+
+// function example(){
+//     var z = 60;
+//     // console.log(z);
+// }
+// example();
+// console.log(z);
+
+console.log(b);
+
+console.log(typeof(z));
+console.log(typeof(a));
