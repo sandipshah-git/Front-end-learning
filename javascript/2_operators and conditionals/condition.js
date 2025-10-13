e9888bd6426f80eb9a2427dda1b23f9ee6ab9c62
sandipsah-git <sandip123456sah@gmail.com>
2025-10-14 04:53:15 +0545

    commit

---
+++ javascript/2_operators and conditionals/condition.js
@@ -0,0 +1,28 @@
+
+// let speed = 87;
+
+// if(speed > 90){
+//     console.log("OverSpeeding");
+// }
+
+// if(speed > 50 && speed <=90){
+//     console.log("Normal");
+// }
+
+// if(speed <= 50){
+//     console.log("Slow");
+// }
+
+
+let num = 4;
+switch(num){
+    case 1 : console.log("A");
+    break;
+    case 2 : console.log("B");
+    break;
+    case 3 : console.log("C");
+    break;
+    case 4 : console.log("D");
+    break;
+    default : console.log("z");
+}
