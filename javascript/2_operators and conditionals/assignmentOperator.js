81767b96bd95d61d6db7ba9609d92f458e402013
sandipsah-git <sandip123456sah@gmail.com>
2025-10-13 06:10:37 +0545

    strict and quality comparare operators

--- javascript/2_operators and conditionals/assignmentOperator.js
+++ javascript/2_operators and conditionals/assignmentOperator.js
@@ -1,2 +1,25 @@
 // Assignment Operators
 // The assignment operator assigns value to the variables. The most common assignment operator we use is '='.
+
+
+let firstName = "Sandip";
+let z = 4;
+
+console.log(firstName);      // Sandip
+console.log(z);              // 4
+
+// Comparison Operators   
+//  (>),  (<), (>=), (<=),  (==), (!=) are used to compare values and determine the relationship between them. 
+// These return a boolean value in the output.
+
+//   Strict Equality v/s Loose Equality
+
+let x = "10";
+console.log(typeof(x));        // string
+
+let y = 10;
+console.log(typeof(y));         // number
+
+console.log(x == y);            // true  (compare only value)
+
+console.log(x === y);           // false  (compare value and data type also)
