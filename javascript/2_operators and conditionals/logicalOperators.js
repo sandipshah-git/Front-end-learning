60c9066dc6f1d47bc0cec36912379d0a459498a5
sandipsah-git <sandip123456sah@gmail.com>
2025-10-13 08:31:48 +0545

    commit

---
+++ javascript/2_operators and conditionals/logicalOperators.js
@@ -0,0 +1,26 @@
+// logical operator
+
+// let ans  = (true && false && true);  // if anyone is false then result will false
+// let ans = (false || false || true); // if anyone is true then result will true
+// let ans = !(false);
+
+// let ans = (1 && 0);  // 0
+// let ans =  (1 || 0);     // 1
+
+// console.log(ans);
+
+
+
+
+// let alert2;
+// let alert3 = NaN;  // (not a number )
+// let mySum = 45;
+
+
+// console.log(!alert2);      // true
+// console.log(!alert3);      // true
+// console.log(!mySum);       // false
+
+
+
+
