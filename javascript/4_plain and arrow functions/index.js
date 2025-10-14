fc873a9c291819b68f274c2339d41864f0401a35
sandipsah-git <sandip123456sah@gmail.com>
2025-10-14 06:26:31 +0545

    commit

--- javascript/4_plain and arrow functions/index.js
+++ javascript/4_plain and arrow functions/index.js
@@ -1,1 +1,28 @@
-console.log("sand")
+// function
+
+// function functionName (){
+//     console.log("love you !");
+// }
+
+// functionName();
+
+// function printNumber (num){   // also can write parameter without data type : let , const
+//     for(let i = 0;i<=5; i++);
+//     console.log("Number is : " , num);
+// }
+// printNumber(4); // argument is 4       
+ 
+
+// function Expression
+
+// const functionName = function (a, b){
+//     return a * b;
+// }
+// console.log(functionName(2,5));
+
+
+// Arrow function (use this for best practice)
+const functionName =  (a, b) => {
+    return a * b;
+}
+console.log(functionName(2,5));
