997801bc62d366be298e385fd92dd1c4d7c53ba1
sandipsah-git <sandip123456sah@gmail.com>
2025-10-14 07:18:44 +0545

    commit

---
+++ javascript/5_objects and arrays/array.js
@@ -0,0 +1,9 @@
+// Array is a collections of items
+
+// creation of array
+let arr = [1,2,4,5,6,7];
+console.log(arr);
+
+// array constructor
+let brr = new Array(3, "ram ", true, 'sita',4);
+console.log(brr);
---
+++ javascript/5_objects and arrays/index.js
@@ -0,0 +1,12 @@
+let obj = {
+    Name : "sandip",
+    // "full name" : "sandip shah" // if variable has space the use " " ("full name")
+    age : 23,
+    wight : 62,
+    height : "5' 11 inch ",
+    greet : function( ){
+        console.log("Namaste");
+    }
+};
+console.log(obj);
+obj.greet();
