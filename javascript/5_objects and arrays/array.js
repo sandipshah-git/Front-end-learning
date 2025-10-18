6f99156eaac866584c77a82cfbdd75cca431e00d
sandipsah-git <sandip123456sah@gmail.com>
2025-10-18 18:15:52 +0545

    commit

--- javascript/5_objects and arrays/array.js
+++ javascript/5_objects and arrays/array.js
@@ -140,4 +140,25 @@
 
 //   arrays with function
 
+let arr = [10, 20, 30, 40, 50];
+
+// function getSum(arr){
+//     let len = arr.length;
+//     let sum = 0;
+//     for(let i = 0; i < len; i++){
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+function getSum(arr){
+    let sum = 0;
+    arr.forEach((value)=>{
+        sum += value;
+    })
+    return sum;
+}
+
+
+let result = getSum(arr);
+console.log(result);
 
