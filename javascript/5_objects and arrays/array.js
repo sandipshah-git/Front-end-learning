256d6c39a6726cc6fbc25c084d19cd8d3b20986b
sandipsah-git <sandip123456sah@gmail.com>
2025-10-18 14:48:32 +0545

    commit

--- javascript/5_objects and arrays/array.js
+++ javascript/5_objects and arrays/array.js
@@ -6,5 +6,17 @@ console.log(arr);
 
 // array constructor
 let brr = new Array(3, "ram ", true, 'sita',4);
+// brr.push("sandip");  //  goes at last side of array
+// brr.pop();   // it removes last item of array
+// brr.shift();  // removes first item of array
+brr.unshift("sandip");   // add item at first side of array
+// console.log(brr.slice(1,4));   //  taking out some part/slice from array
+// brr.splice(2,4,'shah');  //  'shah' goes at position of 2 index and no. of 4 item will remove  (-> splice can change content or insert,replace,remove
+// brr.splice(3,0,false); // revore 0 no. of items & false goes at 3 index position
+
+
+
 console.log(brr);
+
+// console.log(brr[3]);
 
