61a831ff2ef9dac13d2aefd27b2e3dcb38958fb1
sandipsah-git <sandip123456sah@gmail.com>
2025-10-18 15:47:51 +0545

    commit

--- javascript/5_objects and arrays/array.js
+++ javascript/5_objects and arrays/array.js
@@ -1,22 +1,51 @@
 // Array is a collections of items
 
 // creation of array
-let arr = [1,2,4,5,6,7];
-console.log(arr);
+// let arr = [1,2,4,5,6,7];
+// console.log(arr);
 
 // array constructor
-let brr = new Array(3, "ram ", true, 'sita',4);
+// let brr = new Array(3, "ram ", true, 'sita',4);
 // brr.push("sandip");  //  goes at last side of array
 // brr.pop();   // it removes last item of array
 // brr.shift();  // removes first item of array
-brr.unshift("sandip");   // add item at first side of array
+// brr.unshift("sandip");   // add item at first side of array
 // console.log(brr.slice(1,4));   //  taking out some part/slice from array
 // brr.splice(2,4,'shah');  //  'shah' goes at position of 2 index and no. of 4 item will remove  (-> splice can change content or insert,replace,remove
 // brr.splice(3,0,false); // revore 0 no. of items & false goes at 3 index position
 
-
-
-console.log(brr);
+// console.log(brr);
 
 // console.log(brr[3]);
+
+
+                        // map 
+
+// let arr1 = [10, 20, 30];
+
+// let ansArray = arr1.map( (Number) => {     // map is for functioning of array items
+//     return Number * Number;
+// })
+// console.log(ansArray)
+
+// arr1.map( (Number , index) => {
+//     console.log(Number);
+//     console.log(index);
+// })
+
+          //    filter
+
+// let arr = [2,10,23,13,50,34,26];
+
+// let result = arr.filter((Number)=>{
+//     // if(Number % 2 === 0){
+//     //     return true;
+//     // }
+//     // else{
+//     //     return false;
+//     // }
+//     return Number % 2 ==0;
+// });
+// console.log(result);
+
 
