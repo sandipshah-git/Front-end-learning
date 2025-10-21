3626bfd65ab89e60752a912d00cb14a915baa0c6
sandipsah-git <sandip123456sah@gmail.com>
2025-10-21 08:03:57 +0545

    commit

---
+++ javascript/7_Temporal Dead Zone/index.js
@@ -0,0 +1,64 @@
+// variable scoping (global ,  function , scope)
+
+
+
+// {
+//     const age = 100;
+// }
+
+// console.log(age);
+
+
+// console.log(height);
+// {
+//     var height = 180;
+// }
+
+// console.log(height);
+
+
+// console.log(fullName);
+// function sayHello() {
+//     const fullName = "Earth";
+//     console.log("Hello Dunia ", name);
+// }
+// console.log(fullName);
+// sayHello();
+
+
+
+
+
+// //global scope
+// const age = 15;
+
+// console.log(age);
+
+// {
+//     console.log(age)
+// }
+// if(true ){
+//     console.log(age)
+// }
+// for(let i=0; i<2; i++) {
+//     console.log(age);
+// }
+
+// function sayHello() {
+//     console.log("hi ", age);
+// }
+// sayHello();
+
+
+
+
+
+
+
+//  temporal dead zone
+
+console.log(marks);
+console.log("babbar");
+console.log("Love");
+const marks = 100;
+// console.log(marks);
