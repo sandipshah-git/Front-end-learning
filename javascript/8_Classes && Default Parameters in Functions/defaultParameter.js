6ecc25afb6d9dd8c06860991cc50d37f3e90075f
sandipsah-git <sandip123456sah@gmail.com>
2025-10-22 20:53:30 +0545

    commit

---
+++ javascript/8_Classes && Default Parameters in Functions/defaultParameter.js
@@ -0,0 +1,30 @@
+
+
+
+function getAge() {
+    return 190;
+}
+
+
+function utility(name = "Love", age = getAge()) {
+   console.log(name, " ", age); 
+}
+
+utility();
+
+
+
+
+
+// function solve(value = "Rahul") {
+//     console.log("HELLO JII -> ", value);
+// }
+// solve(undefined);
+// function sayName(fName = "Uday", lName = fName.toUpperCase()) {
+//     console.log("My Name is: ", fName," ", lName);
+// }
+
+// sayName("sandip");
+
+
+
