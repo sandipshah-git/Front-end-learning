08944e0885117ba9541beb24ace8d2c93c68a09b
sandipsah-git <sandip123456sah@gmail.com>
2025-10-22 21:27:49 +0545

    commit

---
+++ javascript/9_Common In-Built Objects/Math.js
@@ -0,0 +1,23 @@
+
+
+// console.log("want you");
+
+// console.log(Math.PI);
+
+// console.log(Math.max(60,30,24,600,712,89));
+
+// console.log(Math.min(60,30,24,600,712,89));
+
+// console.log(Math.round(1.78)); //  round off  is   2
+
+// console.log(Math.floor(3.9));    // print number before decimal (print just smallest value )
+
+// console.log(Math.ceil(1.1));   //    ->  2        print number after decimal  (print just greatest value)  
+
+// console.log(Math.abs(-5));   // absolute makes that value into positive
+
+// console.log(Math.random());   // print random number between 0 and 1
+
+// console.log(Math.sqrt(5));   //  2.23
+
+// console.log(Math.pow(2,10));
